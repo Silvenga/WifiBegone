9470b763c9d80731d9b4f0dce4112b7fb16bb04c
Mark Lopez <m@silvenga.com>
2016-04-29 19:46:04 -0500

    Restart on update.

--- src/WifiBegone.Tray/App.xaml.cs
+++ src/WifiBegone.Tray/App.xaml.cs
@@ -4,40 +4,13 @@ namespace WifiBegone.Tray
 {
     using System.Threading.Tasks;
 
-    using Squirrel;
-
     public partial class App : Application
     {
-        private const string UpdateUrl = "https://github.com/Silvenga/WifiBegone";
-
         public App()
         {
-            using (var mgr = UpdateManager.GitHubUpdateManager(UpdateUrl).Result)
-            {
-                const string exePath = "WifiBegone.Tray.exe";
-                const ShortcutLocation shortcutPaths = ShortcutLocation.StartMenu | ShortcutLocation.Startup;
+            SquirrelManager.StartupEventsAsync().Wait();
 
-                SquirrelAwareApp.HandleEvents(
-                    onInitialInstall:
-                        v =>
-                            mgr.CreateShortcutsForExecutable(exePath, shortcutPaths, false),
-                    onAppUpdate:
-                        v =>
-                            mgr.CreateShortcutsForExecutable(exePath, shortcutPaths, true),
-                    onAppUninstall:
-                        v =>
-                            mgr.RemoveShortcutsForExecutable(exePath, shortcutPaths));
-            }
-
-            Task.Run(async () => await UpdateLoopAsync());
-        }
-
-        private async Task UpdateLoopAsync()
-        {
-            using (var mgr = UpdateManager.GitHubUpdateManager(UpdateUrl))
-            {
-                await mgr.Result.UpdateApp();
-            }
+            Task.Run(async () => await SquirrelManager.UpdateAsync());
         }
     }
 }
---
+++ src/WifiBegone.Tray/SquirrelManager.cs
@@ -0,0 +1,65 @@
+﻿namespace WifiBegone.Tray
+{
+    using System;
+    using System.IO;
+    using System.Reflection;
+    using System.Threading.Tasks;
+
+    using Squirrel;
+
+    public static class SquirrelManager
+    {
+        private const string UpdateUrl = "https://github.com/Silvenga/WifiBegone";
+        private const ShortcutLocation ShortcutPaths = ShortcutLocation.StartMenu | ShortcutLocation.Startup;
+
+        private static string ExePath => Path.GetFileName(Assembly.GetEntryAssembly().Location);
+
+        private static async Task<UpdateManager> CreateUpdateManagerAsync()
+        {
+            var manager = await UpdateManager.GitHubUpdateManager(UpdateUrl);
+            return manager;
+        }
+
+        public static async Task StartupEventsAsync()
+        {
+            using (var mgr = await CreateUpdateManagerAsync())
+            {
+                SquirrelAwareApp.HandleEvents(
+                    onInitialInstall:
+                        v =>
+                            mgr.CreateShortcutsForExecutable(ExePath, ShortcutPaths, false),
+                    onAppUpdate:
+                        v =>
+                            mgr.CreateShortcutsForExecutable(ExePath, ShortcutPaths, true),
+                    onAppUninstall:
+                        v =>
+                            mgr.RemoveShortcutsForExecutable(ExePath, ShortcutPaths));
+            }
+        }
+
+        public static async Task UpdateAsync()
+        {
+            using (var mgr = await CreateUpdateManagerAsync())
+            {
+                var release = await mgr.CheckForUpdate();
+                var version = release.FutureReleaseEntry?.Version;
+
+                if (version != null)
+                {
+                    Console.WriteLine(version);
+                    await ApplyUpdateAsync(version.Version, mgr);
+                }
+            }
+        }
+
+        private static async Task ApplyUpdateAsync(Version lastVersion, UpdateManager mgr)
+        {
+            await mgr.UpdateApp();
+
+            var folder = $"app-{lastVersion.Major}.{lastVersion.Minor}.{lastVersion.Build}";
+            var latestExe = Path.Combine(mgr.RootAppDirectory, folder, ExePath);
+
+            UpdateManager.RestartApp(latestExe);
+        }
+    }
+}
